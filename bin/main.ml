9446462e3fe6da7d50a0a8bed164ec3e499a7d08
Rahul Yedida <yrahul3910@gmail.com>
2026-03-09 14:07:20 -0400

    feat: set up basic cli, implement `read_file`

---
+++ bin/main.ml
@@ -0,0 +1,23 @@
+open Cmdliner
+
+let file =
+  let doc = "The DreamBerd source file to execute." in
+  Arg.(value & pos 0 (some string) None & info [] ~docv:"FILE" ~doc)
+
+let verbose =
+  let doc = "Print internal interpreter state." in
+  Arg.(value & flag & info ["v"; "verbose"] ~doc)
+
+let run file verbose =
+  match file with
+  | None -> print_endline "No file provided!"
+  | Some f -> 
+        Printf.printf "Interpreting %s (verbose: %b)\n" f verbose;
+        Printf.printf "Contents: %s" (Interpreter.Files.read_file f)
+
+let cmd =
+  let doc = "A DreamBerd interpreter" in
+  let info = Cmd.info "dreamberd" ~doc in
+  Cmd.v info Term.(const run $ file $ verbose)
+
+let () = exit (Cmd.eval cmd)
