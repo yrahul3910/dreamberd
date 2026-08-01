d54a7b9b5ffaa50b493737c3328c7f3e0817108e
Rahul Yedida <yrahul3910@gmail.com>
2026-07-31 23:33:22 -0400

    fix: fix length check for function keyword

--- src/scanner.rs
+++ src/scanner.rs
@@ -202,8 +202,10 @@ fn get_keyword_token(word: &str) -> Option<TokenType> {
 ///
 /// `true` if this is a function keyword.
 fn is_function_keyword(word: &str) -> bool {
+    const MIN_FUNCTION_KW_LEN: usize = 1;
     let re = Regex::new("f?u?n?c?t?i?o?n?").unwrap();
-    re.find(word).is_some_and(|m| m.len() == word.len())
+
+    word.len() >= MIN_FUNCTION_KW_LEN && re.find(word).is_some_and(|m| m.len() == word.len())
 }
 
 /// Parse a number from a string that is either a hex value, oct value, or a valid number.
